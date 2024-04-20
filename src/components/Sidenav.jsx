1f225430daad65e33577817cca5ad8da937f4fab
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-04-20 09:58:14 +0500

    Update SideNav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -7,6 +7,7 @@ import {
   Stack,
   Text,
 } from "@chakra-ui/react";
+import { BiSupport } from "react-icons/bi";
 import { BsArrowDownUp } from "react-icons/bs";
 import { RxDashboard } from "react-icons/rx";
 
@@ -24,24 +25,48 @@ const Sidenav = () => {
     },
   ];
   return (
-    <Stack boxShadow="sm" maxW="16rem" h="100vh">
-      <Heading textAlign="center" fontSize="20px" as="h1" pt="3.5rem">
-        Crypto Dashboard
-      </Heading>
-      <Box mt="6" mx="3">
-        {navLinks.map((nav) => (
-          <HStack
-            key={nav.text}
-            py="3"
-            px="4"
-            _hover={{
-              bg: "#F3F3F7",
-            }}
-          >
-            <Icon as={nav.icon} />
-            <Text fontSize="14px">{nav.text}</Text>
-          </HStack>
-        ))}
+    <Stack justify="space-between" boxShadow="lg" maxW="16rem" h="100vh">
+      <Box>
+        <Heading textAlign="center" fontSize="20px" as="h1" pt="3.5rem">
+          Crypto Dashboard
+        </Heading>
+        <Box mt="6" mx="3">
+          {navLinks.map((nav) => (
+            <HStack
+              borderRadius="10px"
+              key={nav.text}
+              py="3"
+              px="4"
+              _hover={{
+                bg: "#F3F3F7",
+                color: "#171717",
+              }}
+              color="#797E82"
+            >
+              <Icon as={nav.icon} />
+              <Text fontSize="14px" fontWeight="medium">
+                {nav.text}
+              </Text>
+            </HStack>
+          ))}
+        </Box>
+      </Box>
+      <Box mt="6" mx="3" mb="6">
+        <HStack
+          borderRadius="10px"
+          py="3"
+          px="4"
+          _hover={{
+            bg: "#F3F3F7",
+            color: "#171717",
+          }}
+          color="#797E82"
+        >
+          <Icon as={BiSupport} />
+          <Text fontSize="14px" fontWeight="medium">
+            Support
+          </Text>
+        </HStack>
       </Box>
     </Stack>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/ubuntu";
+import "@fontsource/ubuntu/400.css";
+import "@fontsource/ubuntu/500.css";
+import "@fontsource/ubuntu/700.css";
 import { theme } from "./theme/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
