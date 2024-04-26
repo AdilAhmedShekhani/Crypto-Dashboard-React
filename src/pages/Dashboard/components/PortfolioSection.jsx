f078586cf1dfd81345c537626b256f2a2276f8e4
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-04-26 11:54:26 +0500

    UI Updated

--- src/pages/Dashboard/components/PortfolioSection.jsx
+++ src/pages/Dashboard/components/PortfolioSection.jsx
@@ -5,26 +5,64 @@ import { FaArrowDown, FaArrowUp } from "react-icons/fa6";
 
 const PortfolioSection = () => {
   return (
-    <HStack justify="space-between" bg="white" borderRadius="xl" p="6">
-      <HStack spacing={16}>
+    <HStack
+      justify="space-between"
+      bg="white"
+      borderRadius="xl"
+      p="6"
+      align={{
+        base: "flex-start",
+        xl: "start",
+      }}
+      flexDir={{
+        base: "column",
+        xl: "row",
+      }}
+      spacing={{
+        base: 4,
+        xl: 0,
+      }}
+    >
+      <HStack
+        spacing={{
+          base: 0,
+          xl: 16,
+        }}
+        align={{
+          base: "flex-start",
+          xl: "start",
+        }}
+        flexDir={{
+          base: "column",
+          xl: "row",
+        }}
+      >
         <Stack>
           <HStack color="black.80">
             <Text fontSize="sm">Total Portfolio Value</Text>
             <Icon as={AiOutlineInfoCircle} />
           </HStack>
-          <Text textStyle="h2">Rs. 112,786.36</Text>
+          <Text textStyle="h2" fontWeight="medium">
+            Rs. 112,786.36
+          </Text>
         </Stack>
         <Stack>
           <HStack color="black.80">
             <Text fontSize="sm">Wallet Balances</Text>
           </HStack>
-          <HStack>
+          <HStack spacing={4}>
             <HStack>
-              <Text textStyle="h2">22.39401000</Text>{" "}
-              <Tag colorScheme="gray">BTC</Tag>
+              <Text textStyle="h2" fontWeight="medium">
+                22.39401000
+              </Text>{" "}
+              <Tag colorScheme="gray" fontWeight="medium">
+                BTC
+              </Tag>
             </HStack>
             <HStack>
-              <Text textStyle="h2">Rs. 1,280.00</Text>{" "}
+              <Text textStyle="h2" fontWeight="medium">
+                Rs. 1,280.00
+              </Text>{" "}
               <Tag colorScheme="gray">PKR</Tag>
             </HStack>
           </HStack>
