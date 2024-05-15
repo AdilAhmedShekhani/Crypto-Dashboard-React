270ea0e7263d170372f5865ff8daf3e1a5f49679
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-05-15 09:15:50 +0500

    Routes Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import Dashboard from "./pages/Dashboard/Dashboard"
+import Dashboard from "./pages/Dashboard/Dashboard";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Transactions from "./pages/Dashboard/components/Transactions";
+import Support from "./pages/Support/Support";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/transactions",
+    element: <Transactions />,
+  },
+  {
+    path: "/support",
+    element: <Support />,
+  },
+]);
 
 function App() {
-
   return (
     <>
-      <Dashboard/>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
