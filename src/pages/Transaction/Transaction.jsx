3d2c6806f8d6be105457e0365f868096e5ea56e7
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-05-20 16:17:51 +0500

    Transaction Page Updated

--- src/pages/Transaction/Transaction.jsx
+++ src/pages/Transaction/Transaction.jsx
@@ -36,7 +36,7 @@ const TransactionPage = () => {
   ];
 
   return (
-    <DashboardLayout>
+    <DashboardLayout title="Transactions">
       <Flex justify="end" mt="6" mb="3">
         <Button leftIcon={<Icon as={PiDownloadSimpleBold} />}>
           Export CSV
@@ -52,7 +52,7 @@ const TransactionPage = () => {
           >
             <HStack>
               {tabs.map((tab) => (
-                <Tab key={tab.name} display="flex" gap="2">
+                <Tab key={tab.name} display="flex" gap="2" pb="4">
                   {tab.name}{" "}
                   <Tag colorScheme="gray" borderRadius="full">
                     {tab.count}
@@ -61,7 +61,7 @@ const TransactionPage = () => {
               ))}
             </HStack>
 
-            <InputGroup maxWidth="200px">
+            <InputGroup maxWidth="200px" pr="6">
               <InputLeftElement pointerEvents="none">
                 <Icon as={BsSearch} />
               </InputLeftElement>
