2ba0083e32204a848e65104b29189cd39ce18ecf
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-05-23 08:36:10 +0500

    Support Card Responsiveness

--- src/pages/Support/components/SupportCard.jsx
+++ src/pages/Support/components/SupportCard.jsx
@@ -18,8 +18,14 @@ import { IoMdMail } from "react-icons/io";
 
 const SupportCard = () => {
   return (
-    <Flex>
-      <Stack>
+    <Flex
+      gap={6}
+      flexDir={{
+        base: "column",
+        xl: "row",
+      }}
+    >
+      <Stack maxW="24rem">
         <Icon as={IoMdMail} boxSize={6} color="p.purple" />
         <Text fontWeight="medium" as="h1" textStyle="h1">
           Contact Us
@@ -29,12 +35,17 @@ const SupportCard = () => {
           us.
         </Text>
       </Stack>
-      <Card p="6" borderRadius="1rem">
+      <Card p="6" borderRadius="1rem" flexGrow={1}>
         <Stack spacing={6}>
           <Text fontWeight="medium" fontSize="sm">
             You will receive respone within 24 hours of time of submit.
           </Text>
-          <HStack>
+          <HStack
+            flexDir={{
+              base: "column",
+              md: "row",
+            }}
+          >
             <FormControl>
               <FormLabel>Name</FormLabel>
               <Input placeholder="Enter Your Name..." />
@@ -52,15 +63,19 @@ const SupportCard = () => {
             <FormLabel>Message</FormLabel>
             <Textarea placeholder="Enter Your Message..." />
           </FormControl>
-          <Checkbox defaultChecked fontSize="xs">
-            I agree with
-            <Box as="span" color="p.purple">
-              Terms & Conditions.
-            </Box>
+          <Checkbox defaultChecked>
+            <Text fontSize="xs">
+              I agree with
+              <Box as="span" color="p.purple">
+                Terms & Conditions.
+              </Box>
+            </Text>
           </Checkbox>
           <Stack>
-            <Button>Send a Message</Button>
-            <Button colorScheme="gray">Book a Meeting</Button>
+            <Button fontSize="sm">Send a Message</Button>
+            <Button fontSize="sm" colorScheme="gray">
+              Book a Meeting
+            </Button>
           </Stack>
         </Stack>
       </Card>
